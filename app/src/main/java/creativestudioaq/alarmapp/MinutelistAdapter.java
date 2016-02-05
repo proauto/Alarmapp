82bf07d7c28e6bc53ffa140676c944545691596f
proauto <hg1771@naver.com>
2016-02-06 02:13:04 +0900

    CodeArrange

---
+++ app/src/main/java/creativestudioaq/alarmapp/MinutelistAdapter.java
@@ -0,0 +1,64 @@
+package creativestudioaq.alarmapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by honggyu on 2016-02-05.
+ */
+public class MinutelistAdapter extends BaseAdapter {
+
+
+    private LayoutInflater _inflater;
+    private static ArrayList<Repeatlist> _lists;
+    private int _layout;
+    private static Context m_ctx;
+
+
+    public MinutelistAdapter(Context context, int layout, ArrayList<Repeatlist> lists) {
+        _inflater = (LayoutInflater) context.getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE);
+        _lists = lists;
+        _layout = layout;
+        m_ctx = context;
+
+    }
+
+    @Override
+    public int getCount() {
+        return _lists.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        convertView = _inflater.inflate(_layout, parent, false);
+
+
+        TextView textlist = (TextView) convertView.findViewById(R.id.textlist);
+        RadioButton buttonlist = (RadioButton) convertView.findViewById(R.id.buttonlist);
+
+        textlist.setText(_lists.get(position).gettime());
+        buttonlist.setChecked(_lists.get(position).getcheck());
+
+
+        return convertView;
+    }
+
+}
